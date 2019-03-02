332daf6607445c0e68c3ef05507fa6ac6a9e35a0
ashok204 <aashok417@gmail.com>
2019-03-02 16:55:05 +0530

    my basic loop programs

---
+++ Basicsofjava.java
@@ -0,0 +1,86 @@
+package com.regnant;
+
+public class Basicsofjava {
+
+	public static void main(String[] args) {
+		//if
+		int m=45;
+		if(m>=45) {
+			System.out.println("pass");
+		}
+//else if
+		int n=50;
+		if(m>=50) {
+			System.out.println("pass");
+			
+		}
+		
+		else {
+			System.out.println("fail");
+		}
+	//else if
+		int i=70;
+		if(i<=60) {
+			System.out.println("1st class");
+		}
+		else if(i<=50) {
+			System.out.println("2nd class");
+			
+		}
+		
+		else if(i<45) {
+			System.out.println("pass");
+				}
+		else {
+			System.out.println("fail");
+		}
+	//for loop
+		int j;
+		for(j=1;j<=10;j++) {
+			System.out.println(" raja="+j);
+		}
+		//while loop
+		int k=0;
+		while(k<=10) {
+			System.out.println(k);
+			k++;
+		}
+	//do while loop
+		int b=1;
+		do {
+		System.out.println(b);
+		b++;
+		}
+	while(b<=10);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+}
