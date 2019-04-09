8b1dd8fce80de14a5f2e2afb673ba8b8fa02ec9b
ashok204 <47915587+ashok204@users.noreply.github.com>
2019-04-09 12:51:39 +0530

    Iteratorvector

---
+++ Iteratorvector.java
@@ -0,0 +1,17 @@
+package com.collections;
+
+import java.util.Vector;
+
+public class Iteratorvector {
+	Vector<String>vectorlist=new Vector<String>();
+	void vectordemo() {
+		vectorlist.add("kumar");
+		vectorlist.add("hani");
+		vectorlist.add("indu");
+		vectorlist.add("jack");
+		vectorlist.add("peter");
+System.out.println("vector values"+vectorlist);
+		
+	}
+
+}
